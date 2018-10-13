ade984f66fbcefacd498bb7c25eed0d10d7fed8f
wjy <stephenwang11@qq.com>
2018-10-13 19:10:33 +0800

    新建一个用来保存笔试题的项目

---
+++ src/com/xxx/bishi/Main1.java
@@ -0,0 +1,13 @@
+package com.xxx.bishi;
+
+import java.util.Scanner;
+
+public class Main1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+	}
+
+}
